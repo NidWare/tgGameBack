9615a78cb23fe50e15b865a9c1f6c07d95427063
Leonid Sukhinin <leonsukh@gmail.com>
2024-07-12 13:07:31 +0300

    upd

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import user_routes
-from database import create_tables
+from .routes import user_routes  # Use relative import
+from .database import create_tables
 
 app = FastAPI()
 
